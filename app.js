1c4a0b96313315bf417e8e239c089d5ac2091502
sithu-nanda <sithuyannainglwm@gmail.com>
2023-12-02 14:22:16 +0630

    fourth commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const header = document.querySelector("header h3")
 const dates = document.querySelector(".dates")
+const navis = document.querySelectorAll("i");
 let date = new Date()
 const months = [
     "January",
@@ -21,7 +22,7 @@ let year = date.getFullYear()
 //Dates[1 - 31]
 function renderCalender(){
     header.innerHTML = `${months[month]} ${year}`;
-    
+    let dateHTML = "";
     //first end dates of prev month render
 
     //currentMonthfirstDay
@@ -32,7 +33,7 @@ function renderCalender(){
     console.log(prevMonthLastDay)
 
     for(let i = currentMonthFirstDay ; i>0 ;i--){
-        dates.innerHTML += `<li>${prevMonthLastDay - i +1}</li>`
+        dateHTML += `<li class = "inactive">${prevMonthLastDay - i +1}</li>`
     }
 
 
@@ -40,9 +41,52 @@ function renderCalender(){
 
     //currentMonthendDate
     const currentMonthEndDate = new Date(year,month +1,0).getDate();
-    for(let i=1; i<currentMonthEndDate;i++){
-        dates.innerHTML += `<li>${i}</li>`
+    for(let i=1; i<=currentMonthEndDate;i++){
+        dateHTML += `<li>${i}</li>`
     }
 
+    //nextmonthstartdate
+    const curMonthEndDay = new Date(year,month,currentMonthEndDate).getDay()
+
+    for(let i = 1 ; i <= 6-curMonthEndDay; i++){
+        dateHTML += `<li class= "inactive">${i}</li>`
+    }
+    dates.innerHTML = dateHTML;
+
 }
+navis.forEach(arr=>{
+    arr.addEventListener('click',(e=>{
+        arrclassName =e.target.className;
+        console.log(arrclassName)
+        if(arrclassName==="fa-solid fa-chevron-left"){
+            if(month>0){
+                month -= 1; 
+            }
+            else{
+                year --;
+                month = 11;
+            }
+            console.log(month)
+        }
+        else if(arrclassName === "fa-solid fa-chevron-right"){
+            if(month<11){
+                month += 1;
+            }else{
+                year ++;
+                month = 0;
+            }
+
+        }
+
+        date = new Date(year, month, new Date().getDate())
+        year = date.getFullYear()
+        month = date.getMonth();
+        
+        renderCalender();
+
+    }))
+})
+
+
+
 renderCalender()
